38fcbd883ea3170832b4a77f913e4054712a0037
Taiga Hashimoto <2002421taiga@gmail.com>
2023-06-17 21:28:32 +0900

    6.17.21

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,32 @@
 const { environment } = require('@rails/webpacker')
 
+const customConfig = {
+    resolve: {
+      fallback: {
+        dgram: false,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false
+      }
+    }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
